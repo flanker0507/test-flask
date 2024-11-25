0cbb909c1f59f940a89cf967692ff141ce720e38
flanker0507 <yudawirapratama05@gmail.com>
2024-11-25 13:00:12 +0700

    D O N E

--- Main.py
+++ Main.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from crypt import methods
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def main():
+    web_title = "Halaman Utama"
+    return render_template('home.html',web_title = web_title)
+
+@app.route("/about")
+def about():
+    web_title = "Halaman About"
+    return render_template('about.html',web_title = web_title)
+
+@app.route("/usia", methods=['GET', 'POST'])
+def cek_usia():
+    if request.method == 'POST':
+        tahun_lahir = int(request.form['tahun_lahir'])
+        tahun_sekarang = 2024
+        usia = tahun_sekarang - tahun_lahir
+        return render_template('cek_usia.html', usia = usia)
+    return render_template('cek_usia.html', usia = None)
 
 if __name__ == "__main__":
     app.run()
